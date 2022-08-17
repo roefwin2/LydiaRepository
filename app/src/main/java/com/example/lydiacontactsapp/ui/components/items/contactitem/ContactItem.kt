87ca60ccaba99999a9749f3beac47bde503c45d8
Reg_Dk <regis.dika@etu.unice.fr>
2022-08-18 00:18:51 +0200

    adjust UI

--- app/src/main/java/com/example/lydiacontactsapp/ui/components/items/contactitem/ContactItem.kt
+++ app/src/main/java/com/example/lydiacontactsapp/ui/components/items/contactitem/ContactItem.kt
@@ -1,12 +1,15 @@
 package com.example.lydiacontactsapp.ui.components.items.contactitem
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.lydiacontactsapp.data.local.entity.LydiaContactEntity
@@ -34,11 +37,11 @@ fun Contact(contact: LydiaContactEntity, onClickListener: (id : Int?) -> Unit) {
                     .padding(4.dp)
             )
             Column() {
-                Text(text = "Contact ${contact.firstName}!")
+                Text(text = "Firstname ${contact.firstName}")
                 Spacer(modifier = Modifier.padding(2.dp))
-                Text(text = "Number ${contact.lastName}!")
-                Spacer(modifier = Modifier.padding(2.dp))
-                Text(text = "Number ${contact.phone}!")
+                Text(text = "Lastname ${contact.lastName}")
+                Spacer(modifier = Modifier.padding(10.dp))
+                Text(text = "Number ${contact.phone}",Modifier.border(BorderStroke(2.dp, Color.DarkGray)))
             }
 
         }
